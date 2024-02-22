042c2f0d9f2b355ee04e1496200a35b678fbae9d
Gabriel Bezerra <gabriel.bezerra@tcsindustrial.com.br>
2024-02-22 11:08:31 -0300

    user rg na procuracao

--- src/app/criar-procuracao/criar-procuracao.component.ts
+++ src/app/criar-procuracao/criar-procuracao.component.ts
@@ -2,21 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { EnderecoService } from '../services/enderecoService/endereco.service';
 import { AutomacoesService } from '../services/automacoes/automacoes.service';
 import Swal from 'sweetalert2';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-criar-procuracao',
   templateUrl: './criar-procuracao.component.html',
-  styleUrls: ['./criar-procuracao.component.scss']
+  styleUrls: ['./criar-procuracao.component.scss'],
+  animations: [
+    trigger('onEnter', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('onLeave', [
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('300ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
+
+
 export class CriarProcuracaoComponent implements OnInit{
+
+  identificacao = 'cpf';
+
   procuracao = {
     nomeCliente : "",
     isCasado: "",
     cpfCliente: "",
+    rgCliente: "",
     ruaCliente: "",
     estadoCliente: "",
     cepCliente: "",
     cidadeDocumento: "",
+    identificacao: "",
   };
 
   constructor(
@@ -30,6 +52,7 @@ export class CriarProcuracaoComponent implements OnInit{
   }
 
   criarProcuracao() {
+    this.procuracao.identificacao = this.identificacao;
     this.automacoesService.getProcuracaoWord(this.procuracao).subscribe(resp => {
       Swal.fire({
         icon: "success",
@@ -80,6 +103,27 @@ export class CriarProcuracaoComponent implements OnInit{
     }
   }
 
+  changeIdentificacao(identificacao: string) {
+    this.identificacao = identificacao;
+  }
+
+  applyRgMask(event: any): void {
+    let value = event.target.value;
+    // Remove tudo o que não for dígito
+    value = value.replace(/\D/g, '');
+
+    // Limita a 9 dígitos para o RG
+    value = value.substring(0, 9);
+
+    // Aplica a máscara do RG: XX.XXX.XXX-X
+    value = value.replace(/(\d{2})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+    event.target.value = value;
+  }
+
+
   applyCpfMask(event: any): void {
     let value = event.target.value;
     value = value.replace(/\D/g, ''); // Remove tudo o que não for dígito
@@ -103,6 +147,22 @@ export class CriarProcuracaoComponent implements OnInit{
     value = value.substring(0, 8);
 
     value = value.replace(/^(\d{5})(\d)/, '$1-$2'); // Coloca hífen após o quinto dígito
+    event.target.value = value;
+  }
+
+  applyCnpjMask(event: any): void {
+    let value = event.target.value;
+    value = value.replace(/\D/g, ''); // Remove tudo o que não for dígito
+
+    // Limita a 14 dígitos para o CNPJ
+    value = value.substring(0, 14);
+
+    // Aplica a máscara do CNPJ
+    value = value.replace(/(\d{2})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1.$2');
+    value = value.replace(/(\d{3})(\d)/, '$1/$2');
+    value = value.replace(/(\d{4})(\d{1,2})$/, '$1-$2');
+
     event.target.value = value;
   }
 }
