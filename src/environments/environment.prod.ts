cb581f16a95d288332139b0206ffa70fbd60d113
devgpb <bezerra.pereira.dev@gmail.com>
2024-10-08 12:29:03 -0300

    unfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiURL: "http://18.231.3.169/api",
-  socketURL:"http://18.231.3.169"
+	apiURL: "https://18.231.3.169/api",
+  socketURL:"https://18.231.3.169"
 };
