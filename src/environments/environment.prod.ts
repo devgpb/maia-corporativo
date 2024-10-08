0945d1c44a179c0e2433c132a08a7704c01bc7da
devgpb <bezerra.pereira.dev@gmail.com>
2024-10-08 12:05:10 -0300

    fix api temp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiURL: "https://18.231.3.169/api",
-  socketURL:"https://18.231.3.169"
+	apiURL: "http://18.231.3.169/api",
+  socketURL:"http://18.231.3.169"
 };
