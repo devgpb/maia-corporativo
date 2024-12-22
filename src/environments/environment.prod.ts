2c669d8595d98f47916fe8da9b2b8928389098a4
Gabriel_Pereira_B <gabriel.bezerra@tcsindustrial.com.br>
2024-12-22 02:56:33 -0300

    Corrigindo enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
 	production: true,
 	apiURL: "https://api-maia.com/api",
   socketURL:"https://api-maia.com",
-  formFechado: "https://forms.gle/AR3d5nJVD5S1QkZ48",
+  linkFormFechado: "https://forms.gle/AR3d5nJVD5S1QkZ48",
 };
